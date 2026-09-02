7bf9366cff77d31074e52f67e3d224c5ebd65bdf
M-Taha4x <muhammadtaha6002@gmail.com>
2026-09-02 23:06:00 +0500

    Fixed Bug for Tansfers

--- app.py
+++ app.py
@@ -41,47 +41,46 @@ with tab1:
     st.metric("Current Balance",f"Rs {current_balance:,.2f}")
 
 with tab2:
-    st.subheader("Add  Transaction")
-    with st.form("add_transaction_form"):
-        date=st.date_input("Date")
-        account_name=st.selectbox("Account",account_df['name'])
-        amount=st.number_input("Amount",step=1.0)
-        type_=st.selectbox("Type",['spend','income','transfer','loan_given','loan_repaid'])
-        if type_=='transfer':
-            st.write("Transfer Between Accounts")
-            col1,col2=st.columns(2)
+    st.subheader("Add Transaction")
+    
+    type_ = st.selectbox("Type", ['spend', 'income', 'transfer', 'loan_given', 'loan_repaid'])
+    
+    if type_ == 'transfer':
+        st.write("Transfer Between Accounts")
+        with st.form("add_transfer_form"):
+            date = st.date_input("Date")
+            col1, col2 = st.columns(2)
             with col1:
-                from_account_name=st.selectbox("From",account_df['name'])
+                from_account_name = st.selectbox("From", account_df['name'])
             with col2:
-                to_account_name=st.selectbox("To",account_df['name'])
+                to_account_name = st.selectbox("To", account_df['name'])
+            transfer_amount = st.number_input("Transferred Amount", min_value=0.0, step=1.0)
+            note = st.text_input("Note")
             
-            transfer_amount=st.number_input("Transfered Amount",min_value=0.0,step=1.0)
-            note=st.text_input("Note")
-            submitted=st.form_submit_button("Add Transfer")
+            submitted = st.form_submit_button("Add Transfer")
             if submitted:
-                if from_account_name==to_account_name:
-                    st.error("From and To Accounts must be different")
+                if from_account_name == to_account_name:
+                    st.error("From and To accounts must be different")
                 else:
-                    from_id=int(account_df[account_df['name'] == from_account_name]['account_id'].iloc[0])
-                    to_id=int(account_df[account_df['name'] == to_account_name]['account_id'].iloc[0])
-                    db.insert_transfer(str(date),from_id,to_id,transfer_amount,note)
+                    from_id = int(account_df[account_df['name'] == from_account_name]['account_id'].iloc[0])
+                    to_id = int(account_df[account_df['name'] == to_account_name]['account_id'].iloc[0])
+                    db.insert_transfer(str(date), from_id, to_id, transfer_amount, note)
                     st.cache_data.clear()
                     st.success("Transfer Added")
-        else:
-            category_name=st.selectbox("Category",categories_df['name'])
-            note=st.text_input("Note")
-    
-            submitted=st.form_submit_button("Add Transaction")
+    else:
+        with st.form("add_transaction_form"):
+            date = st.date_input("Date")
+            account_name = st.selectbox("Account", account_df['name'])
+            amount = st.number_input("Amount", step=1.0)
+            category_name = st.selectbox("Category", categories_df['name'])
+            note = st.text_input("Note")
+            
+            submitted = st.form_submit_button("Add Transaction")
             if submitted:
-                matching_row = account_df[account_df['name'] == account_name]
-                account_id = int(matching_row['account_id'].iloc[0])
-                category_id =int(categories_df[categories_df['name'] == category_name]['category_id'].iloc[0])
+                account_id = int(account_df[account_df['name'] == account_name]['account_id'].iloc[0])
+                category_id = int(categories_df[categories_df['name'] == category_name]['category_id'].iloc[0])
                 if type_ == 'spend' and amount > 0:
                     amount = -amount
-    
-        #st.write("DEBUG account_id:", account_id, type(account_id))
-        #st.write("DEBUG category_id:", category_id, type(category_id))
-    
                 db.insert_transaction(str(date), account_id, amount, type_, category_id, note)
                 st.cache_data.clear()
                 st.success("Transaction Added!")
